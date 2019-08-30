073142369d5f48d784ca6194d0def125c369c0f2
Wiput Pootong <wiput.pootong@gmail.com>
2019-08-31 01:22:42 +0700

    :sparkles: add index route

--- src/main/java/me/wiput/sop/smart_shop/SmartShopApplication.java
+++ src/main/java/me/wiput/sop/smart_shop/SmartShopApplication.java
@@ -2,12 +2,20 @@ package me.wiput.sop.smart_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SmartShopApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SmartShopApplication.class, args);
+  }
+
+  @RequestMapping("/")
+  String index() {
+    return "Hi!";
   }
 
 }
