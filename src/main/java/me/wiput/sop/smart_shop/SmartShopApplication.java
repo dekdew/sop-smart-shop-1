31651c32e31c76e55f845cf0001b0206754e9a7e
Wiput Pootong <wiput.pootong@gmail.com>
2019-08-31 01:09:13 +0700

    :tada: initial commit

---
+++ src/main/java/me/wiput/sop/smart_shop/SmartShopApplication.java
@@ -0,0 +1,13 @@
+package me.wiput.sop.smart_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmartShopApplication.class, args);
+  }
+
+}
